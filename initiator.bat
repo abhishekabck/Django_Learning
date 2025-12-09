db879fa05a3b6918c99c5706f962fc63b5b4b644
Abhishek Chaurasiya <abhiabckchaurasiya@gmail.com>
2025-12-09 11:15:52 +0530

    updated initiatorFile

--- initiator.bat
+++ initiator.bat
@@ -1,3 +1,9 @@
-C:\Users\abhis\PycharmProjects\Django_learning\.venv\Scripts\activate.bat
+@echo off
+echo Step1: Setting up virtual environment
+call C:\Users\abhis\PycharmProjects\Django_learning\.venv\Scripts\activate.bat
+
+echo Step2: Moving to original Directory
 cd firstproject
+
+echo Step3: Running the Server
 python manage.py runserver
